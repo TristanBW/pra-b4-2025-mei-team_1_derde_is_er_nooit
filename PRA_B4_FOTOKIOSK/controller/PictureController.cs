3773b8fcf1b56868770036a66aed727450716ea4
C6je in je kontgat <159583352+Pandoraxx1@users.noreply.github.com>
2025-06-04 10:49:39 +0200

    A1

--- PRA_B4_FOTOKIOSK/controller/PictureController.cs
+++ PRA_B4_FOTOKIOSK/controller/PictureController.cs
@@ -1,11 +1,8 @@
-﻿using PRA_B4_FOTOKIOSK.magie;
+using PRA_B4_FOTOKIOSK.magie;
 using PRA_B4_FOTOKIOSK.models;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PRA_B4_FOTOKIOSK.controller
 {
@@ -14,43 +11,55 @@ namespace PRA_B4_FOTOKIOSK.controller
         // De window die we laten zien op het scherm
         public static Home Window { get; set; }
 
-
         // De lijst met fotos die we laten zien
         public List<KioskPhoto> PicturesToDisplay = new List<KioskPhoto>();
-        
-        
+
         // Start methode die wordt aangeroepen wanneer de foto pagina opent.
         public void Start()
         {
+            // Huidige dagnummer ophalen (0 = zondag, 6 = zaterdag)
+            int today = (int)DateTime.Now.DayOfWeek;
 
-            // Initializeer de lijst met fotos
-            // WAARSCHUWING. ZONDER FILTER LAADT DIT ALLES!
-            // foreach is een for-loop die door een array loopt
-            foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
+            // Pad naar de foto's map
+            string basePath = @"../../../fotos";
+
+            // Doorloop alle dag-mappen (bijv. 0_Zondag, 1_Maandag, ...)
+            foreach (string dir in Directory.GetDirectories(basePath))
             {
-                /**
-                 * dir string is de map waar de fotos in staan. Bijvoorbeeld:
-                 * \fotos\0_Zondag
-                 */
-                foreach (string file in Directory.GetFiles(dir))
+                // Mapnaam ophalen, bijvoorbeeld "2_Dinsdag"
+                string dirName = Path.GetFileName(dir);
+
+                // Probeer het dagnummer (voor de "_") uit te lezen
+                string[] parts = dirName.Split('_');
+                if (parts.Length > 0 && int.TryParse(parts[0], out int dirDayNumber))
                 {
-                    /**
-                     * file string is de file van de foto. Bijvoorbeeld:
-                     * \fotos\0_Zondag\10_05_30_id8824.jpg
-                     */
-                    PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+                    // Alleen de map van vandaag gebruiken
+                    if (dirDayNumber == today)
+                    {
+                        // Voeg alle foto's toe uit deze map
+                        foreach (string file in Directory.GetFiles(dir))
+                        {
+                            PicturesToDisplay.Add(new KioskPhoto()
+                            {
+                                Id = 0,
+                                Source = file
+                            });
+                        }
+                    }
                 }
             }
 
-            // Update de fotos
+            // Update de foto's op het scherm
             PictureManager.UpdatePictures(PicturesToDisplay);
         }
 
         // Wordt uitgevoerd wanneer er op de Refresh knop is geklikt
         public void RefreshButtonClick()
         {
-
+            // Leeg de huidige lijst
+            PicturesToDisplay.Clear();
+            // Herstart de filtering
+            Start();
         }
-
     }
 }
